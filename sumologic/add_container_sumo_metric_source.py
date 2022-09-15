e77e59793b0917350f881e04e5b284eb84a215a8
Gabryel Monteiro <gabryelarm@gmail.com>
2022-09-15 03:23:24 -0300

    Adding args to add container sumo metric source

--- sumologic/add_container_sumo_metric_source.py
+++ sumologic/add_container_sumo_metric_source.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
 from sumo_functions import write_sources, write_user_properties
 
@@ -21,12 +22,15 @@ fields = {
     "vpc": vpc
 }
 
-write_sources(f'{env}/{service_type}/{vpc}/{alias}', metricSources=metric_sources)
+path = sys.argv[0] if len(sys.argv) > 0 else '/opt/SumoCollector/config'
+
+write_sources(f'{env}/{service_type}/{vpc}/{alias}', metricSources=metric_sources, path=path)
 write_user_properties(
     f'{env}-{service_type}-{vpc}-{alias}',
     access_id,
     access_key,
     fields,
     ephemeral=True,
-    java_wrapper="/opt/SumoCollector/jre/bin/java"
+    java_wrapper="/opt/SumoCollector/jre/bin/java",
+    path=path
 )
